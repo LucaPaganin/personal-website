75ce742614339fa757c039557c50ebc5858d91b8
Luca Paganin <luca.paganin1994@gmail.com>
2025-03-24 22:23:02 +0100

    new footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,10 +1,20 @@
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+
 export default function Footer() {
   return (
     <footer className="mt-10 py-6 text-center bg-gray-200 dark:bg-gray-800">
       <p className="text-gray-700 dark:text-gray-300">Find me on:</p>
       <ul className="flex justify-center space-x-4 mt-2">
-        <li><a href="https://www.linkedin.com/in/YOUR_PROFILE" className="text-blue-600 hover:underline">LinkedIn</a></li>
-        <li><a href="https://github.com/YOUR_GITHUB" className="text-gray-900 dark:text-gray-300 hover:underline">GitHub</a></li>
+        <li>
+          <a href="https://www.linkedin.com/in/luca-paganin-phd-1a0b1a160" className="text-blue-600 hover:underline">
+            <FaLinkedin size={24} />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/LucaPaganin" className="text-gray-900 dark:text-gray-300 hover:underline">
+            <FaGithub size={24} />
+          </a>
+        </li>
       </ul>
     </footer>
   );
